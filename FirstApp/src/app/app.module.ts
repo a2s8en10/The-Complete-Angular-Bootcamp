f80b75387d2f3600e2f442e35a5d8a714c226499
Anurag Sahu <anu.sahu9589@gmail.com>
2024-03-10 11:01:33 +0530

    Add code

--- FirstApp/src/app/app.module.ts
+++ FirstApp/src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { UserListComponent } from './user-list/user-list.component';
+import { UserAuthModule } from './user-auth/user-auth.module';
+import { InstagramModule } from './instagram/instagram.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    UserListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserAuthModule,
+    InstagramModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FirstApp/src/app/instagram/sign-up/sign-up.component.spec.ts
+++ FirstApp/src/app/instagram/sign-up/sign-up.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UserListComponent } from './user-list.component';
+import { SignUpComponent } from './sign-up.component';
 
-describe('UserListComponent', () => {
-  let component: UserListComponent;
-  let fixture: ComponentFixture<UserListComponent>;
+describe('SignUpComponent', () => {
+  let component: SignUpComponent;
+  let fixture: ComponentFixture<SignUpComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserListComponent ]
+      declarations: [ SignUpComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UserListComponent);
+    fixture = TestBed.createComponent(SignUpComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
